d3338e5b986976745b193332770951fe930078e2
vinod salunke <salunkevinod45@gmail.com>
2026-01-26 19:54:58 +0530

    remove connection added

--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch, useSelector } from "react-redux";
 import { addConnections } from "../utils/connectionSlice";
@@ -8,6 +8,9 @@ const Connections = () => {
   const storeConnections = useSelector(
     (state) => state.connections.connections,
   );
+  const [removeConnection, setRemoveConnection] = useState(null);
+  const [error, setError] = useState(null);
+
   const dispatch = useDispatch();
 
   const getConnections = async () => {
@@ -19,49 +22,95 @@ const Connections = () => {
       dispatch(addConnections(connections.data.data));
     } catch (error) {
       console.log(error);
+      setError(error.message);
     }
   };
   useEffect(() => {
     getConnections();
   }, []);
+
+  const handleRemoveConnection = async (id) => {
+    try {
+      const removeConnection = await axios.delete(
+        `${BASE_URL}removeConnection/${id}`,
+        { withCredentials: true },
+      );
+      if (removeConnection) {
+        const updatedConnections = storeConnections.filter(
+          (connection) => connection.connectionId !== id,
+        );
+        dispatch(addConnections(updatedConnections));
+        setRemoveConnection("connection removed successfully");
+        setTimeout(() => {
+          setRemoveConnection(null);
+        }, 3000);
+      }
+    } catch (error) {
+      setError(error?.response?.data);
+      setTimeout(() => {
+        setError(null);
+      }, 3000);
+    }
+  };
   if (!storeConnections || storeConnections.length === 0) {
     return <div>No connections</div>;
   }
   return (
     <>
       {storeConnections && (
-        <div className="grid grid-cols-4 gap-5 mb-20">
-          {storeConnections.map((connection, index) => {
-            const {
-              firstName,
-              lastName,
-              gender,
-              email,
-              about,
-              skills,
-              photoUrl,
-              age,
-            } = connection.user;
-            return (
-              <div className="card bg-base-100 w-96 shadow-sm">
-                <figure>
-                  <img src={photoUrl} alt="Shoes" />
-                </figure>
-                <div className="card-body">
-                  <h2 className="card-title">{firstName + " " + lastName}</h2>
-                  <div className="italic">{gender + "," + age}</div>
-                  <p>{about}</p>
-                  <div className="card-actions justify-end">
-                    <button className="btn btn-primary">
-                      Remove Connection
-                    </button>
+        <>
+          {removeConnection && (
+            <div className="toast toast-start z-50">
+              <div className="alert alert-success">
+                <span>{removeConnection}</span>
+              </div>
+            </div>
+          )}
+
+          {error && (
+            <div className="toast toast-start z-50">
+              <div className="alert alert-error">
+                <span>{error}</span>
+              </div>
+            </div>
+          )}
+
+          <div className="grid grid-cols-4 gap-5 mb-20">
+            {storeConnections.map((connection, index) => {
+              const {
+                firstName,
+                lastName,
+                gender,
+                email,
+                about,
+                skills,
+                photoUrl,
+                age,
+              } = connection.user;
+              const { connectionId } = connection;
+              return (
+                <div className="card bg-base-100 w-96 shadow-sm">
+                  <figure>
+                    <img src={photoUrl} alt="Shoes" />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title">{firstName + " " + lastName}</h2>
+                    <div className="italic">{gender + "," + age}</div>
+                    <p>{about}</p>
+                    <div className="card-actions justify-end">
+                      <button
+                        className="btn btn-primary"
+                        onClick={() => handleRemoveConnection(connectionId)}
+                      >
+                        Remove Connection
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
 
-          {/* <div className="card bg-base-100 w-96 shadow-sm">
+            {/* <div className="card bg-base-100 w-96 shadow-sm">
             <figure>
               <img
                 src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
@@ -155,7 +204,8 @@ const Connections = () => {
               </div>
             </div>
           </div> */}
-        </div>
+          </div>
+        </>
       )}
     </>
   );
